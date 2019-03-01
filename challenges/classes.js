e57b837a1a741098ab879875a5bcf08b857d3661
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-01 12:56:40 -0500

    Completed stretch goals

--- challenges/classes.js
+++ challenges/classes.js
@@ -21,17 +21,43 @@ class CuboidMaker2 {
 }; // CuboidMaker
     
   
+ class cubeMaker extends CuboidMaker {
+    constructor(cubeAtt){
+        super(cubeAtt)
+    } 
+    
+    cubeVolume() {
+        return console.log(this.length * this.length * this.length)
+    }
+
+    cubeSurfaceArea(){
+        return console.log(6 * (this.length * this.length))
+    }
+ }
+
+
 
   cuboid = new CuboidMaker2 ({
     length: 4,
     width: 5,
     height: 5
   });
+
+  cube = new cubeMaker({
+      length: 3
+  })
+
+  cube2 = new cubeMaker ({
+      length: 2
+  })
   
   
   console.log(cuboid.volume()); // 100
   console.log(cuboid.surfaceArea()); // 130
-  
+  cube.cubeVolume();
+  cube.cubeSurfaceArea();
+  cube2.cubeVolume();
+  cube2.cubeSurfaceArea();
   
 
  
